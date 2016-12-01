c8677d201a6cf56805e0c7086d2158cb9aad37e8
Abtin Ghods <abtin.ghods@gmail.com>
2016-12-01 16:56:22 -0500

    Add more fields to toggle config

--- src/feature-toggle.config.ts
+++ src/feature-toggle.config.ts
@@ -1,10 +1,17 @@
 export const featureToggleConfig = {
   'new-formula': {
     isEnabled: true,
-    description: 'Change by the factor of 5. It intends to replace OldFormula.',
+    description:
+      `Change by the factor of 5. 
+      It intends to replace OldFormula with NewFormula.`,
     shouldExpireAt: '08-13-2016', // other possible name is deadline
-    responsible: 'Abtin Ghods', //The engineer/team responsible to monitor and remove the toggle. Usually, this responsibility is given to the one which have chosen to add it.
-    cleanUpHowTo: ''
+    // the engineer/team responsible to monitor and remove the toggle. Usually,
+    // this responsibility is given to the one which have chosen to add it.
+    responsible: 'Abtin Ghods',
+    cleanUpHowTo: '',
+    // if this feature is among the features that are designed to interact with
+    //  each other
+    group: null,
   }
 };
 
