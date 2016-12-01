c85b527614cbce6ecd2a085ec14b3227c6cd5e1b
Abtin Ghods <abtin.ghods@gmail.com>
2016-12-01 17:18:38 -0500

    Remove the Formula toggle
    As the toggle responsibility was moved to the toggle-router
    And now the toggling happens in the Formula index

--- src/services/formula/formula.toggle.ts
+++
@@ -1,6 +0,0 @@
-import { NewFormula } from './new-formula';
-import { OldFormula } from './old-forumla/';
-import { toggleRouter } from '../toggle-router/toggle-router';
-
-export const Formula =
-  toggleRouter.getFeature('new-formula', NewFormula, OldFormula);
