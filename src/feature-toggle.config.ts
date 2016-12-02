fc0ba4c5f42718646d3f624e3c5647f1025bb314
Abtin Ghods <abtin.ghods@gmail.com>
2016-12-02 16:12:29 -0500

    Add more info to the toggle config

--- src/feature-toggle.config.ts
+++ src/feature-toggle.config.ts
@@ -8,11 +8,14 @@ export const featureToggleConfig = {
     // the engineer/team responsible to monitor and remove the toggle. Usually,
     // this responsibility is given to the one which have chosen to add it.
     responsible: 'Abtin Ghods',
-    cleanUpHowTo: '',
+    cleanUpHowTo: `
+      in the formula/index.ts
+      remove the toggleRouter.getFeature and instead get the NewFormula directly
+      delete the old-formula directory
+      remove the OldFormula reference from the
+    `,
     // if this feature is among the features that are designed to interact with
     //  each other
     group: null,
-  }
+  },
 };
-
-// Todo: move the config into a YAML file
