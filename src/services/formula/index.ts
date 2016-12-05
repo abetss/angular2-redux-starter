74fa42dd045e594a5cb2509892162148cbd9b46f
Abtin Ghods <abtin.ghods@gmail.com>
2016-12-05 14:08:06 -0500

    Use constant for toggle name instead of string

--- src/services/formula/index.ts
+++ src/services/formula/index.ts
@@ -1,6 +1,7 @@
 import { NewFormula } from './new-formula';
 import { OldFormula } from './old-forumla/';
 import { toggleRouter } from '../toggle-router/toggle-router';
+import { NEW_FEATURE } from '../toggle-router/toggles';
 
 export const Formula =
-  toggleRouter.getFeature('new-formula', NewFormula, OldFormula);
+  toggleRouter.getFeature(NEW_FEATURE, NewFormula, OldFormula);
