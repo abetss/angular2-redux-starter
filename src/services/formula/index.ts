6966140d58c958941b701deef563bc7bf6b31eb5
Abtin Ghods <abtin.ghods@gmail.com>
2017-01-18 11:21:52 -0500

    Changing the feature toggle integration to a more readable version.
    Removed tests

--- src/services/formula/index.ts
+++ src/services/formula/index.ts
@@ -1,3 +1,7 @@
+import { NewFormula } from '../formula/new-formula';
+import { DefaultFormula } from '../formula/default-formula/';
+import { NEW_FORMULA_TOGGLE } from '../toggle-router/toggles';
 import { toggleRouter } from '../toggle-router/toggle-router';
 
-export const Formula = toggleRouter.createFormula();
+export const Formula =
+  toggleRouter.getFeature(NEW_FORMULA_TOGGLE, NewFormula, DefaultFormula);
--- src/services/toggle-router/toggle-router.ts
+++ src/services/toggle-router/toggle-router.ts
@@ -1,22 +1,17 @@
 // doesn't work when running tests
-let featureToggleConfig = require('./../../../feature-toggle.yml');
-// import { featureToggleConfig } from '../../toggle-config';
-import { NewFormula } from '../formula/new-formula';
-import { DefaultFormula } from '../formula/default-formula/';
-import { NEW_FORMULA_TOGGLE } from './toggles';
+// let featureToggleConfig = require('./../../../feature-toggle.yml');
+import { featureToggleConfig } from '../../toggle-config';
 
 export function createToggleRouter(featureConfig = {}) {
   const featureIsEnabled = featureName =>
     !featureConfig[featureName] ? false : featureConfig[featureName].isEnabled;
-  const getFeature = (newFeatureName, newFeature, defaultFeature) =>
-    featureIsEnabled(newFeatureName) ? newFeature : defaultFeature;
 
   return {
     setFeature(featureName, isEnabled) {
       featureConfig[featureName].isEnabled = isEnabled;
     },
-    createFormula() {
-      return getFeature(NEW_FORMULA_TOGGLE, NewFormula, DefaultFormula);
+    getFeature(newFeatureName, newFeature, defaultFeature) {
+      return featureIsEnabled(newFeatureName) ? newFeature : defaultFeature;
     }
   };
 }
